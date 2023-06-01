fdd0e18132e39da66afd69f6829c060e4a498a0c
Carolina Valladares <carolinalimacvalladares@gmail.com>
2023-06-01 16:28:19 -0300

    chore: separate layouts for admin and site

---
+++ app/(admin)/layout.tsx
@@ -0,0 +1,43 @@
+import "../admin.css";
+import { Montserrat } from "next/font/google";
+import AdminHeader from "./AdminHeader";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+
+export const metadata = {
+  icons: {
+    icon: "/_next/static/media/metadata/larFraternoIconBlack.png",
+  },
+  title: "Lar Fraterno de Cambinda",
+  description: "Plataforma de Conteúdo do Lar Fraterno de Cambinda",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          charSet="UTF-8"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+        />
+      </head>
+      <body className={montserrat.className}>
+        <AdminHeader />
+        <div className="text-white">
+          <div className="max-w-screen-lg m-auto max-lg:px-4">{children}</div>
+        </div>
+      </body>
+    </html>
+  );
+}
