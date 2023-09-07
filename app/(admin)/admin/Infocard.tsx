44c3263e1fd05d462ed11dabfcd0fe957bd6f62d
Carolina Valladares <carolinalimacvalladares@gmail.com>
2023-09-07 13:06:24 -0300

    feat: add infocard spinner

--- app/(admin)/admin/Infocard.tsx
+++ app/(admin)/admin/Infocard.tsx
@@ -1,9 +1,20 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { Loader } from "lucide-react";
+
 type Props = {
   title: string;
   number?: number;
   Icon: JSX.Element;
 };
 export default function Infocard({ title, number, Icon }: Props) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, [number]);
+
   return (
     <div className="p-4 rounded-md shadow-md bg-white flex-1">
       <div>
@@ -13,7 +24,18 @@ export default function Infocard({ title, number, Icon }: Props) {
           </div>
           <p className="text-xl font-semibold ">{title}</p>
         </div>
-        <p className="text-3xl font-bold ml-4">{number ? number : 0}</p>
+
+        <p className="text-3xl font-bold ml-4">
+          {loading ? (
+            <span className="animate-spin block w-fit py-2">
+              <Loader strokeWidth={1.5} />
+            </span>
+          ) : number ? (
+            number
+          ) : (
+            0
+          )}
+        </p>
       </div>
     </div>
   );
